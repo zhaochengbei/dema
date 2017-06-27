ca8c3b65c31932ee39a44a2bdc7279a237d163d7
[zhaochengbei] <[254130645@qq.com]>
2017-06-27 15:47:30 +0800

    optimization code

--- dema-tcp/src/test/java/io/dema/tcp/TcpServerTest.java
+++ dema-tcp/src/test/java/io/dema/tcp/TcpServerTest.java
@@ -25,22 +25,26 @@ import io.dema.tcp.TcpServer;
 public class TcpServerTest 
 {
 
+	/**
+	 * 
+	 */
+	static private TcpServer tcpServer = new TcpServer();
+	/**
+	 * 
+	 */
 	static private IoHandler ioHandler = new IoHandler() {
 		
 		public void onAccept(TcpConnection connection){
 			System.out.println("s_onAccept,"+connection.socket);
 		}
-		
 		public void onRead(TcpConnection connection){
 
 			while(true){
 				System.out.println("s_onRead,"+connection.socket);
 				ByteBuffer data = null;
-				
 				try {
-					ConnectionUtils.readPacket(connection, 0,4, 4,256);
+					data = ConnectionUtils.readPacket(connection, 0,4, 4,256);
 				} catch (TcpException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				if(data == null){
@@ -48,43 +52,25 @@ public class TcpServerTest
 				}
 				data.flip();
 				connection.writeAndFlush(data);
-			}
-			
+			}	
 		}
-		
 		public void onReadIdle(TcpConnection connection) {
-			//direct close
 			connection.close(TcpConnectionCloseReason.ReadIdleTimeOut);
 		}
 		public void onClose(TcpConnection connection,String reason){
 			System.out.println("s_onClose,reason="+connection.closeReason+","+connection.socket);
 		}
-
-		
 	};
 	
 	
-	static private TcpServer tcpServer;
     public static void main( String[] args )
     {
     	try {
-    		tcpServer = new TcpServer();
-    		tcpServer.config(100000,12);
+    		tcpServer.config(100000,12);//100 000 is max connection count ,12 is read idle timeout time unit is second
     		tcpServer.start(9090, ioHandler);
-
-            System.out.println( "Hello World!" );
-//        	Thread.sleep(6000);
-//        	tcpServer.shutdown();
-        	
+            System.out.println( "server started" );        	
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    	//10秒后关闭；
-    }
-    static public ByteBuffer getTestPacket(){
-    	ByteBuffer buffer = ByteBuffer.allocate(5);
-		buffer.putInt(1);
-		buffer.put(Byte.valueOf("1"));
-		return buffer;
     }
 }
